44457cdc7a7230a13dcee044bfc0e645e15f80ce
Prerna Barde <137737291+prerna-barde26@users.noreply.github.com>
2023-10-20 08:53:31 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import './Home.css'
+
+function Home() {
+      const[city, setCity] = React.useState('pune')
+      const[temperature,setTemperature] = useState('0')
+      const [message, setMessage] = useState('')
+
+     async function loadWeatherInfo(){
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f652964084c552e8c0492237a3fabd9c`)
+            setTemperature((response.data.main.temp -273).toFixed(2))
+            setMessage('✅Date fetched successfully')
+        }
+        catch(err){
+            setTemperature(0)
+            setMessage('city not found')
+        }
+            
+     }
+     useEffect(()=>{
+        loadWeatherInfo()
+     },[city])
+
+
+
+    return (
+        <div>
+            
+            <h1 className="app-title"> Weather App For {city}</h1>
+            
+            <input
+             className="search-bar" 
+             type="text"
+              placeholder="Enter City"
+              value={city}
+              onChange={(e)=>{
+                 setCity(e.target.value)
+              }}
+               />
+               <p className="message-text">{message}</p>
+
+
+
+
+
+            <h1 className="temperature-text">Temperature : {temperature} °C</h1>
+         
+
+
+            </div>
+    )
+}
+
+export default Home
